1ae60d3b9747e126d3f19aacc64f426d837bac1f
katherine <kretamales280@gmail.com>
2019-02-22 16:41:27 -0300

    Obteniendo la ruta

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+const path = require('path');
+
+
+
+const argument = process.argv
+
+
+
+dir = argument[2];
+
+console.log(fs.lstatSync(dir));
+
+
+//console.log("Es una carpeta " + fs.lstatSync(dir).isDirectory());
+ 
+
+
